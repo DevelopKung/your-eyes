59e99991311da5562ffcd0f518e4031a98e569b2
atiwat.sut <atiwat.sut@gmail.com>
2023-12-16 20:50:29 +0700

    update

--- layouts/blank.vue
+++ layouts/blank.vue
@@ -4,14 +4,6 @@
   </v-app>
 </template>
 
-<script>
-export default {
-  data () {
-    return {   
-    }
-  } ,
-}
-</script>
 <style lang="scss">
   v-app , .v-application { 
     min-height:100%;
